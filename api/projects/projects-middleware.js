9199db6b13eba2e68bbd006396948d498f4a3a35
Angelique Abacajan <ma_utsey@yahoo.com>
2021-12-03 13:45:45 -0500

    Implemented Projects-Middleware to validate the project's id and the required metadata && implemented get projects by id endpoint

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -1,1 +1,38 @@
 // add middlewares here related to projects
+const Projects = require("../projects/projects-model");
+
+async function validateProjectId( req, res, next ) {
+    try {
+        const { id } = req.params;
+        const projects = await Projects.get(id);
+
+        if(projects) {
+            req.params = projects;
+            next();
+
+        } else {
+            res.status(404).json( { message: "404 Error: Project not found" } )
+        }
+    } catch (error) {
+        next(error);
+    }
+}
+
+async function validateProject(req, res,next){
+    const {name, description, completed} = req.body
+    if(!name || !name.trim()){
+        res.status(400).json({message: 'Name field required'})
+    } else if(!description || !description.trim()){
+        res.status(400).json({message: 'missing required description field'})
+    } else{
+        req.name = name.trim()
+        req.description = description.trim()
+        req.completed = completed
+        next()
+    }
+}
+
+module.exports = {
+    validateProjectId,
+    validateProject
+};
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -4,12 +4,48 @@ const router = express.Router();
 
 const Projects = require("./projects-model");
 
-router.get("/", async ( req, res, next ) => {
-    try {
-        const projects = await Projects.get();
-        res.status(200).json(projects)
+const { validateProjectId, validateProject } = require("./projects-middleware");
 
-    } catch (error) {
-        next(error);
+// [GET] /api/projects
+router.get("/", async ( req, res ) => {
+    try{
+        const projects = await Projects.get();
+        res.status(400).json(projects);
+    }
+    catch(err){
+        res.status(500).json({
+            message: "There was an issue accessing the server with your information"
+        })
     }
 })
+
+// [GET] /api/projects/:id
+router.get("/:id/", async ( req, res, next ) => {
+    const { id } = req.params
+    try {
+        const project = await Projects.get(Number(id))
+        if(!project) {
+            res.status(404).json({
+                message: "Project Not Found"
+            })
+        } else {
+            res.status(200).json(project)
+        }
+    } catch (error) {
+        next(error)
+    }
+})
+
+// [POST] /api/projects
+
+
+
+// [PUT] /api/projects/:id
+
+
+
+// DELETE] /api/projects/:id
+
+
+
+// [GET] /api/projects/:id/actions
