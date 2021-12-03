3081fbea44ec1edc6ee64667f41679a87b0e4fc4
Angelique Abacajan <ma_utsey@yahoo.com>
2021-12-03 15:46:08 -0500

    Implemented validateActionsId middleware

--- api/actions/actions-middlware.js
+++ api/actions/actions-middlware.js
@@ -1,1 +1,25 @@
 // add middlewares here related to actions
+const Actions = require("./actions-model");
+
+async function validateActionId( req, res, next ) {
+    try {
+        const { id } = req.params;
+        const actions = await Actions.get(id);
+
+        if(actions) {
+            req.params = actions;
+            next();
+
+        } else {
+            res.status(404).json( { message: "404 Error: Action ID not found" } )
+        }
+    } catch (error) {
+        next(error);
+    }
+}
+
+
+
+module.exports = {
+    validateActionId
+};
